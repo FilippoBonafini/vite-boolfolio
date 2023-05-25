dd56cb59ef1e5965983fe1b2d971b930b1698903
FilippoBonafini <filippo.bonafini.99@icloud.com>
2023-05-25 22:37:43 +0200

    Creata gestione delle pagine

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ export default {
       apiUrls: {
         projects: 'projects'
       },
+      next_page_url: '',
+      previous_page_url: '',
       projects: []
     }
   },
@@ -21,9 +23,33 @@ export default {
   methods: {
     getProjects(url) {
       axios.get(this.apiBaseUrl + url).then((response) => {
-        console.log(response)
         this.projects = response.data.results;
-        console.log(this.projects)
+        this.next_page_url = response.data.results.next_page_url
+        if (response.data.results.prev_page_url) {
+          this.previous_page_url = response.data.results.prev_page_url
+        }
+      }).catch((error) => {
+        console.log(error)
+      })
+    },
+    previous_page() {
+      axios.get(this.previous_page_url).then((response) => {
+        this.projects = response.data.results;
+        this.next_page_url = response.data.results.next_page_url
+        if (response.data.results.prev_page_url) {
+          this.previous_page_url = response.data.results.prev_page_url
+        }
+      }).catch((error) => {
+        console.log(error)
+      })
+    },
+    next_page() {
+      axios.get(this.next_page_url).then((response) => {
+        this.projects = response.data.results;
+        this.next_page_url = response.data.results.next_page_url
+        if (response.data.results.prev_page_url) {
+          this.previous_page_url = response.data.results.prev_page_url
+        }
       }).catch((error) => {
         console.log(error)
       })
@@ -39,7 +65,7 @@ export default {
 
 <template>
   <AppHeader />
-  <AppMain :data="projects" />
+  <AppMain :data="projects" @previous_page="previous_page" @next_page="next_page" />
   <AppFooter />
 </template>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,12 @@
 <template>
     <main class="container">
         <div class=" my-4 gy-4 d-flex justify-content-between">
-            <div><button v-if="this.data.current_page > 1" type="button" class="btn btn-primary">INDIETRO</button></div>
+            <div><button @click="previous_page" v-if="this.data.current_page > 1" type="button"
+                    class="btn btn-primary">INDIETRO</button></div>
             <div>
                 <h3>Page:{{ this.data.current_page }}</h3>
             </div>
-            <div><button type="button" class="btn btn-primary">AVANTI</button></div>
+            <div><button @click="next_page" type="button" class="btn btn-primary">AVANTI</button></div>
         </div>
         <div class="row my-2 gy-2">
             <div v-for="project in data.data" class="col col-md-4">
@@ -23,6 +24,16 @@ export default {
     props: {
         data: Object
     },
-    components: { AppCard }
+    components: { AppCard },
+
+    methods: {
+        previous_page() {
+            this.$emit('previous_page');
+        },
+        next_page() {
+            this.$emit('next_page');
+        }
+    },
 }
+
 </script>
